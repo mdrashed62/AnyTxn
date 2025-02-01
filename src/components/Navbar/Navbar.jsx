74777e9aea0f4dcf19fb9a41bf3705e4c5f11a52
rashed <mdrashedbiswas83@gmail.com>
2025-02-01 23:05:45 +0600

    created nav section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
+import navLogo from "../../assets/navLogo.svg";
+
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100 shadow-sm">
+    <div className="bg-base-100">
+      <div className="navbar shadow-sm max-w-6xl mx-auto pr-8">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -12,65 +14,76 @@ const Navbar = () => {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                {" "}
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
                   d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
+                />
               </svg>
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow flex flex-col gap-2"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
+              <li className="dropdown bg-none dropdown-hover">
+                <p tabIndex={0} role="button">
+                  Solutions
+                </p>
+                <ul className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 shadow-sm flex flex-col gap-1">
                   <li>
-                    <a>Submenu 1</a>
+                    <a>AnyCaaS</a>
                   </li>
                   <li>
-                    <a>Submenu 2</a>
+                    <a>AnyBaaS</a>
+                  </li>
+                  <li>
+                    <a>AnyPaaS</a>
                   </li>
                 </ul>
               </li>
               <li>
-                <a>Item 3</a>
+                <a>Services</a>
+              </li>
+              <li>
+                <a>About Us</a>
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <button>
+            <img src={navLogo} alt="" />
+          </button>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
+          <ul className="menu menu-horizontal px-1 flex gap-4 items-center">
+            <li className="dropdown bg-none dropdown-hover">
+              <p tabIndex={0} role="button" className="btn">
+                Solutions
+              </p>
+              <ul className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 pt-2 shadow-sm flex flex-col gap-1">
+                <li>
+                  <a>AnyCaaS</a>
+                </li>
+                <li>
+                  <a>AnyBaaS</a>
+                </li>
+                <li>
+                  <a>AnyPaaS</a>
+                </li>
+              </ul>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              <a>Services</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>About Us</a>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <button className="border border-white hover:bg-white hover:text-blue-500 rounded-sm py-3 px-8 font-bold">
+            Contact Us
+          </button>
         </div>
       </div>
     </div>
